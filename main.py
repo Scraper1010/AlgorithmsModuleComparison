afe5f7b062300505eada086d2b21484fb5d61a17
mo-7amed <me0358433@gmail.com>
2025-04-07 19:25:59 +0200

    JumpSearch has been added

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 import time, threading, queue, sys, os, random
-
+from PyQt5.QtWidgets import (QLabel, QLayout, QPushButton, QLineEdit, QHBoxLayout, QVBoxLayout, QComboBox)
+from PyQt5.QtCore import Qt
 
 SD = os.path.join(os.path.dirname(__file__))
 def clear():os.system('cls') if os.name == 'nt' else os.system('clear')
 new_SD = os.path.join(os.path.dirname(SD), 'dump')
-print(new_SD)
+
 
 
 
 
 
 class Algorithms:
-    def __init__(self, lst= None, value= None):
-        self.lst =lst
+    def __init__(self, lst=None, value=None):
+        self.lst = lst
         self.value = value
-        
+        self.steps = 0
+        self.iteration = 0
+        self.temp = None
 
     def LinearSearch(self): #easy as fuck  
+        pass
+
+    def ExponentialSearch(self): #normal
         pass
 
     def BinarySearch(self): #easy as fuck
@@ -35,34 +41,43 @@ class Algorithms:
                     low = mid +1
                 else:
                     high = mid -1
-        return f'value : {self.value} not found in list' 
-        
-                
-                
-            
-        
+        return f'value : {self.value} not found in list'
     
-
-    def ExponentialSearch(self): #normal
-        pass
-
     def JumpSearch(self): #normal
-        pass
-    
+        
+        import math
+        n = len(self.lst)
+        step = int(math.sqrt(n))
+        prev = 0
+
+        while self.lst[min(step, n) - 1] < self.value:
+            self.steps += 1
+            prev = step
+            step += int(math.sqrt(n))
+            if prev >= n:
+                break
+
+        for i in range(prev, min(step, n)):
+                self.steps += 1
+                if self.lst[i] == self.value:
+                    return self.value, i, -1, "JumpSearch", self.steps 
+        return f"value : {self.value} not found"         
+
+
     def InterpolationSearch(self): #hard
         pass
     
     def BubbleSort(self): #hard #not for larg data set
-         #start the timer
-         for i in range(len(self.lst)-1):
-             self.iteration += 1
-             for j in range (len(self.lst)-i-1): # i here just to shrink the index cuz the every iteratoin will make the last element of index it the largest (just for efficiency)
-                 self.iteration += 1
-                 if self.lst[j] > self.lst[j+1]:
-                     self.lst[j], self.lst[j+1] = self.lst[j+1], self.lst[j]
- 
-         #stop the timer
-         return self.lst , self.iteration #elapsed_time
+        #start the timer
+        for i in range(len(self.lst)-1):
+            self.iteration += 1
+            for j in range (len(self.lst)-i-1): # i here just to shrink the index cuz the every iteratoin will make the last element of index it the largest (just for efficiency)
+                self.iteration += 1
+                if self.lst[j] > self.lst[j+1]:
+                    self.lst[j], self.lst[j+1] = self.lst[j+1], self.lst[j]
+
+        #stop the timer
+        return self.lst , self.iteration #elapsed_time
 
     def InsertoinSort(self): #hard
         pass
@@ -75,16 +90,29 @@ class Algorithms:
 
     #timer method
     
+num = 10000
 
 
+lst = [i for i in range((num)+1)]
+# rlst = [random.randint(1, (num)+1) for _ in range((num)+1)]
 
-lst = [i for i in range((10)+1)]
 
 print("lst is created")
 
-# value, index, elapsed_time, AlgorithmName, steps = Algorithms(lst, 11).BinarySearch()  # Updated method call and variable name
-print(Algorithms(lst, 11).BinarySearch())  # Updated method call and variable name
-# print(f"found value {value} in index {index} with elapsed time {elapsed_time} ms with {AlgorithmName} with {steps} steps")
+# lst , iteration = Algorithms(rlst).BubbleSort()
+# print(f"lst : {lst}\niterations : {iteration}")
 
+print("done")
+lst=[1,2,3,4,5,6,7,8,9,10]
+
+# value, index, elapsed_time, AlgorithmName, steps = Algorithms(lst, 11).JumpSearch()  # Updated method call and variable name
+# print(f"found value {value} in index {index} with elapsed time {elapsed_time} ms with {AlgorithmName} with {steps} steps")
+print(Algorithms(lst, 11).JumpSearch())
 del lst
 print("lst has been deleted")
+
+
+
+
+
+
