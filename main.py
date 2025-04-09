d738958916ad2a96845cd9df96623d77c056438a
unknown <midohatem225@gmail.com>
2025-04-09 20:39:47 +0200

    1- made the new timer StartTimer and StopTimer methods U can get the elapsed time from the queue.

--- main.py
+++ main.py
@@ -19,13 +19,31 @@ class Algorithms:
         self.iteration = 0
         self.temp = None
 
+        self.timer_event = threading.Event()
+        self.timer_event.set()
+        self.elapsed_time = queue.Queue()
+        self.timer_thread = threading.Thread(target=self.__run)
+
+
+
+
     def LinearSearch(self): #easy as fuck  
-        pass
+        steps = 0
+        self.StartTimer()
+        for i in range(len(self.lst)):
+            steps += 1
+            # Check if the current element is the target
+            if self.lst[i] == self.value:
+                self.StopTimer()
+                return self.lst[i] , i, self.elapsed_time.get(), 'LinearSearch', steps
+            
+        return -1  # Return -1 if the target is not found
 
     def ExponentialSearch(self): #normal
         pass
 
     def BinarySearch(self): #easy as fuck
+        self.StartTimer()
         low = 0
         high = len(self.lst) -1
         steps = 0
@@ -33,11 +51,13 @@ class Algorithms:
                 steps += 1
                 mid =(low + high) // 2
                 if self.lst[mid] == self.value:
-                    return self.lst[mid] , mid,-1,'Binary search' , steps 
+                    self.StopTimer()
+                    return self.lst[mid] , mid, self.elapsed_time.get(), 'Binary search', steps 
                 elif self.lst[mid] < self.value:
                     low = mid +1
                 else:
                     high = mid -1
+        
         return f'value : {self.value} not found in list'
     
     def JumpSearch(self): #normal
@@ -86,84 +106,49 @@ class Algorithms:
         pass
 
     #timer method
-    def __init__(self):
-        self.start_time = None
-        self.running_time = None
-        self.elapsed_time = 0
-        self.thread = None
+    
 
-    def Timer_start(self) :
-        if self.start_time is None:
-            self.start_time = time.time()
-            self.running_time = True
-            self.thread = threading.Thread(target=self.update_timer)
-            self.thread.start()
-            print("Timer started")
 
-    def update_timer(self):
-        while self.running_time:
-            self.elapsed_time = time.time() - self.start_time
-            time.sleep(1)
+    def StartTimer(self):
+        self.timer_thread.start()
 
-    def Timer_stop(self) :
-        if self.running_time:
-            self.running_time = False
-            self.thread.join()
-            print(f"Timer stopped.")
-            print(f"Elapsed: {int(self.elapsed_time)} sec {self.get_formatted_time()}")         
+    def StopTimer(self):
+        self.timer_event.clear()
+        self.timer_thread.join()
 
-    def reset_timer(self):
-        if self.running_time:
-            self.Tumer_stop()
-        self.start_time = None
-        self.running_time = False
-        self.elapsed_time = 0
-        self.thread = None
-        print("Timer reset.")
-            
-
-    def get_formatted_time (self):  
-        total_seconds = int(self.elapsed_time)
-        hours = total_seconds // 3600
-        minutes = (total_seconds % 3600) // 60
-        seconds = total_seconds % 60
-        return f"{hours:02}:{minutes:02}:{seconds:02}"
+    def __run(self):
+        self.start_time = time.time()
+    
+        while self.timer_event.is_set():
+            time.sleep((0.01)/10**10)
+        elapsed_ms = float(f"{(time.time() - self.start_time) * 1000:.3f}")
+        self.elapsed_time.put(elapsed_ms)
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-num = 10000
+num =10000000
 lst = [i for i in range((num)+1)]
-# rlst = [random.randint(1, (num)+1) for _ in range((num)+1)]
+# # rlst = [random.randint(1, (num)+1) for _ in range((num)+1)]
 
 
-# print("lst is created")
+# # print("lst is created")
 
-# lst , iteration = Algorithms(rlst).BubbleSort()
-# print(f"lst : {lst}\niterations : {iteration}")
+# # lst , iteration = Algorithms(rlst).BubbleSort()
+# # print(f"lst : {lst}\niterations : {iteration}")
 
-print("done")
-lst=[1,2,3,4,5,6,7,8,9,10]
+# print("done")
+# lst=[1,2,3,4,5,6,7,8,9,10]
 
-# value, index, elapsed_time, AlgorithmName, steps = Algorithms(lst, 11).JumpSearch()  # Updated method call and variable name
-# print(f"found value {value} in index {index} with elapsed time {elapsed_time} ms with {AlgorithmName} with {steps} steps")
-print(Algorithms(lst, 11).JumpSearch())
-del lst
-print("lst has been deleted")
+value, index, elapsed_time, AlgorithmName, steps = Algorithms(lst, num).LinearSearch()  # Updated method call and variable name
+print(f"found value {value} in index {index} with elapsed time {elapsed_time} ms with {AlgorithmName} with {steps} steps")
+value, index, elapsed_time, AlgorithmName, steps = Algorithms(lst, num).BinarySearch()  # Updated method call and variable name
+print(f"found value {value} in index {index} with elapsed time {elapsed_time} ms with {AlgorithmName} with {steps} steps")
+# print(Algorithms(lst, 11).JumpSearch())
+# del lst
+# print("lst has been deleted")
 
 
 
