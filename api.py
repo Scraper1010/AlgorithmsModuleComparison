dab142b4ab4932642076d085bf5541453b0bfcee
Sakr-001 <sakrhamdy22@outlook.com>
2025-05-13 08:28:04 +0300

    api usage simple exp

---
+++ api.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI,Path
+from typing import Optional
+from pydantic import BaseModel
+import json, os
+from Algorithms import Algorithms
+
+dir = os.path.dirname(__file__) 
+with open(os.path.join(dir, 'SP_S.json'), 'r') as f:
+    student_profiles = json.load(f)
+student_ids = [profile['id'] for profile in student_profiles]
+
+algo = Algorithms()
+
+
+app=FastAPI()
+@app.get("/")
+def getitem():
+    return "start"
+@app.get("/get-item/{ID}")
+def getitem(ID:int):
+    algo = Algorithms(lst=student_ids,target=ID)
+    x=algo.BinarySearch()
+    
+    return student_profiles[x]
+
+    
+    
+if "__main__"== "__name__":
+    import uvicorn
+
+    uvicorn.run(app,host="0.0.0.0",port=8000)
+    #uvicorn api:app --reload 
